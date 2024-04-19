3601104002c8856adb0a57e621f53859e5bac1ff
mariakaraush <167077657+mariakaraush@users.noreply.github.com>
2024-04-19 09:51:29 +0000

    .

--- checker.js
+++ checker.js
@@ -9,7 +9,7 @@ function getCurrentDateTimeFormatted() {
   const day = String(currentDate.getDate()).padStart(2, '0');
   const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Adding 1 because months are zero-indexed
   const year = String(currentDate.getFullYear());
-  const hours = String(currentDate.getHours()).padStart(2, '0');
+  const hours = String(currentDate.getHours() + 1).padStart(2, '0');
   const minutes = String(currentDate.getMinutes()).padStart(2, '0');
   const seconds = String(currentDate.getSeconds()).padStart(2, '0');
 
